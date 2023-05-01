ceab39fd57434ddcb90bd02ae8cd251ce7b026ed
Ladányi Bence <ladanyi.bence96@gmail.com>
2023-05-01 20:20:54 +0200

    commit try

---
+++ src/components/heading/Head.js
@@ -0,0 +1,12 @@
+import './Head.css';
+
+function Head() {
+  return (
+    <div className='head flex-column-center'>
+      <div className='head-title'>Curse <span className='lower-1'>of</span> Strahd</div>
+      <div className='under-line'></div>
+    </div>
+  );
+}
+
+export default Head;
